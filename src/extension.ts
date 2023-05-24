0823ffdee506ba62f543aea51978342e0a231108
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-05-24 03:39:54 +0000

    update: Prettierで整形した #18

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,33 @@ import * as fs from "fs";
 import * as encoding from "encoding-japanese";
 
 export function activate(context: vscode.ExtensionContext) {
-	// ファイルを保存した時に、EUC-JPのファイルの全角チルダを波ダッシュに変換する
-	const disposable = vscode.workspace.onDidSaveTextDocument((document) => {
-		if (!isEnabled()) { return; }
+  // ファイルを保存した時に、EUC-JPのファイルの全角チルダを波ダッシュに変換する
+  const disposable = vscode.workspace.onDidSaveTextDocument((document) => {
+    if (!isEnabled()) {
+      return;
+    }
 
-		const filePath = document.uri.fsPath;
+    const filePath = document.uri.fsPath;
 
-		const content = fs.readFileSync(filePath);
+    const content = fs.readFileSync(filePath);
 
-		if (!isEUCJP(content)) { return; }
+    if (!isEUCJP(content)) {
+      return;
+    }
 
-		const convertedString = replaceFullWidthTildeToWaveDash(content);
+    const convertedString = replaceFullWidthTildeToWaveDash(content);
 
-		// 既にファイルを保存しているため、変換後の文字列と変換前の文字列が同じなら何もしない
-		// これをしないと、Ctrl+Sを押しっぱなしにしたときに、上書きできなかったとエラーが出てくる
-		// TODO 全角チルダを波ダッシュに変換した際も前述のエラーを出さないようにしたい
-		if (Buffer.compare(convertedString, content) === 0) { return; }
+    // 既にファイルを保存しているため、変換後の文字列と変換前の文字列が同じなら何もしない
+    // これをしないと、Ctrl+Sを押しっぱなしにしたときに、上書きできなかったとエラーが出てくる
+    // TODO 全角チルダを波ダッシュに変換した際も前述のエラーを出さないようにしたい
+    if (Buffer.compare(convertedString, content) === 0) {
+      return;
+    }
 
-		fs.writeFileSync(filePath, convertedString, { flag: "w" });
-	});
+    fs.writeFileSync(filePath, convertedString, { flag: "w" });
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
@@ -32,9 +38,9 @@ export function activate(context: vscode.ExtensionContext) {
  * @returns `true`: 拡張機能の動作が有効, `false`: 拡張機能の動作が無効
  */
 export function isEnabled(): boolean {
-	const config = vscode.workspace.getConfiguration("waveDashUnify");
+  const config = vscode.workspace.getConfiguration("waveDashUnify");
 
-	return config.get("enable") as boolean;
+  return config.get("enable") as boolean;
 }
 
 /**
@@ -46,7 +52,7 @@ export function isEnabled(): boolean {
  * @returns `true`: EUC-JP, `false`: EUC-JP以外
  */
 export function isEUCJP(str: Buffer): boolean {
-	return encoding.detect(str, "EUCJP") === "EUCJP";
+  return encoding.detect(str, "EUCJP") === "EUCJP";
 }
 
 /**
@@ -56,25 +62,29 @@ export function isEUCJP(str: Buffer): boolean {
  * @returns 変換後の文字列
  */
 export function replaceFullWidthTildeToWaveDash(str: Buffer): Buffer {
-	const fullWidthTilde = Buffer.from([0x8F, 0xA2, 0xB7]);
-	const waveDash = Buffer.from([0xA1, 0xC1]);
+  const fullWidthTilde = Buffer.from([0x8f, 0xa2, 0xb7]);
+  const waveDash = Buffer.from([0xa1, 0xc1]);
 
-	const convertedString: number[] = new Array(str.length);
-	let convertedStringIndex = 0;
+  const convertedString: number[] = new Array(str.length);
+  let convertedStringIndex = 0;
 
-	for (let i = 0; i < str.length; i++) {
-		// 変換したい元の文字列が3byteなので、現在の1byteに加えて2byte以上先があるときだけ変換を行う
-		if ((i + fullWidthTilde.length - 1 < str.length) &&
-			(Buffer.compare(str.slice(i, i + fullWidthTilde.length), fullWidthTilde) === 0)) {
+  for (let i = 0; i < str.length; i++) {
+    // 変換したい元の文字列が3byteなので、現在の1byteに加えて2byte以上先があるときだけ変換を行う
+    if (
+      i + fullWidthTilde.length - 1 < str.length &&
+      Buffer.compare(
+        str.slice(i, i + fullWidthTilde.length),
+        fullWidthTilde
+      ) === 0
+    ) {
+      convertedString[convertedStringIndex++] = waveDash[0];
+      convertedString[convertedStringIndex++] = waveDash[1];
 
-			convertedString[convertedStringIndex++] = waveDash[0];
-			convertedString[convertedStringIndex++] = waveDash[1];
+      i += fullWidthTilde.length - 1;
+    } else {
+      convertedString[convertedStringIndex++] = str[i];
+    }
+  }
 
-			i += fullWidthTilde.length - 1;
-		} else {
-			convertedString[convertedStringIndex++] = str[i];
-		}
-	}
-
-	return Buffer.from(convertedString.slice(0, convertedStringIndex));
+  return Buffer.from(convertedString.slice(0, convertedStringIndex));
 }
