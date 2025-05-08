ff38701a539d5d544e5a0c555209bd032f4dc1db
yutotnh <57719497+yutotnh@users.noreply.github.com>
2025-05-09 06:40:18 +0900

    feat: 最小VS Codeサポートバージョンに着目したテストを追加

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,28 @@
 import * as path from "path";
 
-import { runTests } from "@vscode/test-electron";
+import { runTests, downloadAndUnzipVSCode, TestOptions } from "@vscode/test-electron";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+/**
+ * CLI 引数を解析してオプションを取得する
+ * @returns {Object} CLI 引数のオプション
+ */
+function parseCliArgs() {
+  const argv = yargs(hideBin(process.argv))
+    .option("vscode-version", {
+      type: "string",
+      description: "VS Code のバージョンを指定",
+    })
+    .help("help")
+    .alias("help", "h")
+    .parseSync();
+
+  return {
+    vscodeVersion: typeof argv.vscodeVersion === "string" ? argv.vscodeVersion : undefined,
+  };
+}
+
 
 async function main() {
   try {
@@ -12,8 +34,17 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
-    // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    const { vscodeVersion } = parseCliArgs();
+    const options: TestOptions = { extensionDevelopmentPath, extensionTestsPath };
+    console.log(`Using VS Code version: ${vscodeVersion}`);
+    if (vscodeVersion) {
+      // 指定バージョンのキャッシュがあれば再利用し、なければダウンロード
+      const execPath = await downloadAndUnzipVSCode(vscodeVersion);
+      console.log(`VS Code executable path: ${execPath}`);
+      options.vscodeExecutablePath = execPath;
+    }
+
+    await runTests(options);
   } catch (err) {
     console.error("Failed to run tests", err);
     process.exit(1);
